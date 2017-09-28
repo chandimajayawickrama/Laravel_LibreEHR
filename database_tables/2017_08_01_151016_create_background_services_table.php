6cf45adc7307fa91e1afbeba54b7df79ce931974
Chandima Jayawickrama <chandimaerandani.ce@gmail.com>
2017-09-28 19:33:40 +0530

    Update 2017_08_01_151016_create_background_services_table.php

--- database_tables/2017_08_01_151016_create_background_services_table.php
+++ database_tables/2017_08_01_151016_create_background_services_table.php
@@ -7,7 +7,8 @@ class CreateBackgroundServicesTable extends Migration
 {
     /**
      * Run the migrations.
-     *
+     * creates database table for background_services
+     * @author Chandima Jayawickrama
      * @return void
      */
     public function up()
@@ -15,14 +16,14 @@ class CreateBackgroundServicesTable extends Migration
         Schema::create('background_services',function(Blueprint $table){
 			$table->=engine = 'InnoDB';
 			$table->string('name',31);
-			$table->string('title',127);
+			$table->string('title',127)->comment = "Name for the reports";
 			$table->boolean('active')->default(0);
 			$table->tinyInteger('running')->default(-1);
 			$table->timstamp('next_run');
-			$table->integer('execute_interval')->default(0);
-			$table->string('function',127);
-			$table->string('require_once',255);
-			$table->integer('sort_order');
+			$table->integer('execute_interval')->default(0)->comment = "Minimum number of minutes between function calls, 0 = manual mode";
+			$table->string('function',127)->comment = "Name of the background service";
+			$table->string('require_once',255)->comment = "Include file if necessary";
+			$table->integer('sort_order')->comment = "Lower numbers will be run fast";
 			$table->primary('name');
 		});
     }
