13374f0302f09e80e512c3d0f493bc7926f1f9b6
Chandima Jayawickrama <chandimaerandani.ce@gmail.com>
2017-09-28 20:23:54 +0530

    Update 2017_08_01_151242_create_clinical_plans_rules_table.php

--- database_tables/2017_08_01_151242_create_clinical_plans_rules_table.php
+++ database_tables/2017_08_01_151242_create_clinical_plans_rules_table.php
@@ -7,16 +7,17 @@ class CreateClinicalPlansRulesTable extends Migration
 {
     /**
      * Run the migrations.
-     *
+     * Creates database table for clinical_plans_rules
+     * @author Chandima Jayawickrama
      * @return void
      */
     public function up()
     {
         Schema::create('clinical_plans_rules',fucntion(Blueprint $table){
 			$table->engine = 'InnoDB';
-			$table->string('plan_id',31);
-			$table->string('rule_id',31);
-			$table->primary(array('plan_id','rule_id'));
+			$table->string('plan_id',31)->comment = "Unique and maps to list_options list clinical_plans";
+			$table->string('rule_id',31)->comment = "Unique and maps to list_options list clinical_plans";
+			$table->primary(array('plan_id','rule_id'))->comment = "Primary key";
 			
 			
 			
