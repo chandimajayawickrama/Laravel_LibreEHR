f1a34417fbce420ade73371144f3770bde8687a6
Chandima Jayawickrama <chandimaerandani.ce@gmail.com>
2017-09-28 20:48:19 +0530

    Update 2017_08_01_154444_create_ar_activity_table.php

--- database_tables/2017_08_01_154444_create_ar_activity_table.php
+++ database_tables/2017_08_01_154444_create_ar_activity_table.php
@@ -7,7 +7,8 @@ class CreateArActivityTable extends Migration
 {
     /**
      * Run the migrations.
-     *
+     * Creates database table for ar_activity
+     * @author Chandima Jayawickrama
      * @return void
      */
     public function up()
@@ -16,11 +17,11 @@ class CreateArActivityTable extends Migration
 			$table->engine = 'InnoDB';
 			$table->integer('pid');
 			$table->integer('encounter');
-			$table->increment('sequence_no');
+			$table->increment('sequence_no')->comment = "Auto increment";
 			$table->string('code_type',12);
-			$table->string('code',20);
+			$table->string('code',20)->comment= "empty means claim level";
 			$table->string('modifier',12);
-			$table->integer('payer_type');
+			$table->integer('payer_type')->comment = "0 = pt, 1 = ins1, 2 = ins2, etc";
 			$table->dateTime('post_time');
 			$table->integer('post_user');
 			$table->integer('session_id');
@@ -32,7 +33,7 @@ class CreateArActivityTable extends Migration
 			$table->text('follow_up_note');
 			$table->string('account_code',15);
 			$table->string('reason_code',255);
-			$table->primary([sequence_no, pid, encounter]);
+			$table->primary([sequence_no, pid, encounter])->comment = "Primary key";
 			
 		});
     }
