6e75562d5b9e34f81948108ac822c22fd4ccfabd
Chandima Jayawickrama <chandimaerandani.ce@gmail.com>
2017-09-28 19:26:55 +0530

    Update 2017_08_01_144256_create_audit_master_table.php

--- database_tables/2017_08_01_144256_create_audit_master_table.php
+++ database_tables/2017_08_01_144256_create_audit_master_table.php
@@ -7,7 +7,8 @@ class CreateAuditMasterTable extends Migration
 {
     /**
      * Run the migrations.
-     *
+     * Creates the database table for audit_master
+     * @author Chandima Jayawickrama 
      * @return void
      */
     public function up()
@@ -16,13 +17,13 @@ class CreateAuditMasterTable extends Migration
 			$table->engine = 'InnoDB';
 			$table->bigIncrements('id');
 			$table->bigInteger('pid');
-			$table->bigInteger('user_id');
-			$table->tinyInteger('approval_status');
+			$table->bigInteger('user_id')->comment = "The ID of the user who approves or denies";
+			$table->tinyInteger('approval_status')->comment = "1-Pending,2-Approved,3-Denied,4-Appointment directly updated to calendar table, 5-Cancelled appoinment";
 			$table->text('comments');
 			$table->timestamp('created_time');
 			$table->timestamp('modified_time');
 			$table->string('ip_address',200);
-			$table->typeInteger('type');
+			$table->typeInteger('type')->comment = "1-new patient, 2-existing patient, 3-change is only in the documnet, 4-Patient upload, 5-random key, 10-Appointment";
 			$table->primary('id');
 			
 			
