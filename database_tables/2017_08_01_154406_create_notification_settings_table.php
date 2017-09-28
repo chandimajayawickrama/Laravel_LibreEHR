137448505906f18e769b9745fde3d3c367f6c440
Chandima Jayawickrama <chandimaerandani.ce@gmail.com>
2017-09-28 20:26:00 +0530

    Update 2017_08_01_154406_create_notification_settings_table.php

--- database_tables/2017_08_01_154406_create_notification_settings_table.php
+++ database_tables/2017_08_01_154406_create_notification_settings_table.php
@@ -7,14 +7,15 @@ class CreateNotificationSettingsTable extends Migration
 {
     /**
      * Run the migrations.
-     *
+     * Creates database table for notification_settings
+     * @author Chandima Jayawickrama
      * @return void
      */
     public function up()
     {
         Schema::create('notification_settings', function(Blueprint $table){
 			$table->engine = 'InnoDB';
-			$table->increment('SettingsId');
+			$table->increment('SettingsId')->comment = "Primary key, auto increment";
 			$table->integer('Send_SMS_Before_Hours');
 			$table->integer('Send_Email_Before_Hours');
 			$table->string('SMS_gateway_username',100);
