605d641db5fd948e6d0e55b9544779216d1bbaca
Chandima Jayawickrama <chandimaerandani.ce@gmail.com>
2017-09-28 21:04:47 +0530

    Update 2017_08_01_170055_create_amc_misc_data_table.php

--- database_tables/2017_08_01_170055_create_amc_misc_data_table.php
+++ database_tables/2017_08_01_170055_create_amc_misc_data_table.php
@@ -7,20 +7,21 @@ class CreateAmcMiscDataTable extends Migration
 {
     /**
      * Run the migrations.
-     *
+     * Creates database table for amc_misc_data
+     * @author Chandima Jayawickrama
      * @return void
      */
     public function up()
     {
         Schema::create('amc_misc_data',function(Blueprint $table){
 			$table->engine = 'InnoDB';
-			$table->string('amc_id',31);
+			$table->string('amc_id',31)->comment = "Unique and maps to list_options clinical_rules ";
 			$table->bigInteger('pid');
-			$table->string('map_category',255);
-			$table->bigInteger('map_id');
+			$table->string('map_category',255)->comment = "Maps to an object category (such as prescriptions etc) ";
+			$table->bigInteger('map_id')->comment = "Maps to an object id (such as prescripions id etc)";
 			$table->dateTime('date_created');
 			$table->dateTime('date_completed');
-			$table->primary('amc_id','pid','map_id');
+			$table->primary('amc_id','pid','map_id')->comment = "Primary key";
 			
 			
 		});
