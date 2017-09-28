0986b43c945c4165368f2e1f9806e2cc12ea081d
Chandima Jayawickrama <chandimaerandani.ce@gmail.com>
2017-09-28 20:20:37 +0530

    Update 2017_08_01_151348_create_clinical_rules_log_table.php

--- database_tables/2017_08_01_151348_create_clinical_rules_log_table.php
+++ database_tables/2017_08_01_151348_create_clinical_rules_log_table.php
@@ -7,18 +7,19 @@ class CreateClinicalRulesLogTable extends Migration
 {
     /**
      * Run the migrations.
-     *
+     * Creates database table for clinical_rules_log
+     * @author Chandima Jayawickrama
      * @return void
      */
     public function up()
     {
         Schema::create('clinical_rules_log',function(Blueprint $table){
 			$table->engine = 'InnoDB';
-			$table->bigIncrement('id');
+			$table->bigIncrement('id')->comment = "Primary key, auto increment";
 			$table->timestamp('date');
 			$table->bigInteger('pid')->default(0);
 			$table->bigInteger('uid')->default(0);
-			$table->string('category',255);
+			$table->string('category',255)->comment = "An example category is clinical_reminder_widget";
 			$table->text('value');
 			$table->text('new_value');
 			$table->primary('id');
