fe22f7300c0cf52537b54c5a72d5c498d6a83926
Chandima Jayawickrama <chandimaerandani.ce@gmail.com>
2017-09-28 21:38:51 +0530

    Update 2017_08_01_154508_create_lbf_data_table.php

--- database_tables/2017_08_01_154508_create_lbf_data_table.php
+++ database_tables/2017_08_01_154508_create_lbf_data_table.php
@@ -7,7 +7,8 @@ class CreateLbfDataTable extends Migration
 {
     /**
      * Run the migrations.
-     *
+     * Creates database table for lbf_data
+     * @author Chandima Jayawickrama
      * @return void
      */
     public function up()
@@ -17,7 +18,7 @@ class CreateLbfDataTable extends Migration
 			$table->increment('form_id');
 			$table->string('field_id',31);
 			$table->text('field_value');
-			$table->primary(['form_id','field_id']);
+			$table->primary(['form_id','field_id'])->comment = "Primary key";
 			
 		});
     }
