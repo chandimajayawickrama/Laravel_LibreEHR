569215fb8e49a30b90640052da72c87f4ad0e44b
Chandima Jayawickrama <chandimaerandani.ce@gmail.com>
2017-09-28 21:36:11 +0530

    Update 2017_08_01_151146_create_categories_to_documents_table.php

--- database_tables/2017_08_01_151146_create_categories_to_documents_table.php
+++ database_tables/2017_08_01_151146_create_categories_to_documents_table.php
@@ -7,7 +7,8 @@ class CreateCategoriesToDocumentsTable extends Migration
 {
     /**
      * Run the migrations.
-     *
+     * Creates database table for categories_to_documents
+     * @author Chandima Jayawickrama
      * @return void
      */
     public function up()
@@ -16,7 +17,7 @@ class CreateCategoriesToDocumentsTable extends Migration
 			$table->engine = 'InnoDB';
 			$table->integer('category_id')->default(0);
 			$table->integer('document_id')->default(0);
-			$table->primary(array(`category_id`,`document_id`));
+			$table->primary(array(`category_id`,`document_id`))->comment = "Primary key";
 			
 			
 		});
