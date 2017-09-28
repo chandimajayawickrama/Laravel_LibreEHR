b8d6519269d139b06543fb1a17c21f9a084f5549
Chandima Jayawickrama <chandimaerandani.ce@gmail.com>
2017-09-28 19:40:41 +0530

    Update 2017_08_01_151109_create_categories_table.php

--- database_tables/2017_08_01_151109_create_categories_table.php
+++ database_tables/2017_08_01_151109_create_categories_table.php
@@ -7,14 +7,15 @@ class CreateCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
-     *
+     * Creates database table for categories
+     * @author Chandima Jayawickrama
      * @return void
      */
     public function up()
     {
         Schema::create('categories', function(Blueprint $table){
 			$table->engine = 'InnoDB';
-			$table->integer('id')->default(0);
+			$table->integer('id')->default(0)->comment = "Primary key";
 			$table->string('name',255);
 			$table->string('value',255);
 			$table->integer('parent')->default(0);
