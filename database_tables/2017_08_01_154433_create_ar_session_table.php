3760ac9d33402b9b20bd455db29a90b70509adb3
Chandima Jayawickrama <chandimaerandani.ce@gmail.com>
2017-09-28 20:32:30 +0530

    Update 2017_08_01_154433_create_ar_session_table.php

--- database_tables/2017_08_01_154433_create_ar_session_table.php
+++ database_tables/2017_08_01_154433_create_ar_session_table.php
@@ -7,18 +7,19 @@ class CreateArSessionTable extends Migration
 {
     /**
      * Run the migrations.
-     *
+     * Creates database table for ar_session
+     * @author Chandima Jayawickrama
      * @return void
      */
     public function up()
     {
         Schema::create('ar_session',function(Blueprint $table){
 			$table->engine = 'InnoDB';
-			$table->increment('session_id');
-			$table->integer('payer_id');
-			$table->integer('user_id');
-			$table->boolean('closed');
-			$table->string('reference',255);
+			$table->increment('session_id')->comment = "Primary key, auto increment";
+			$table->integer('payer_id')->comment = " 0 = pt else references insurance_companies.id";
+			$table->integer('user_id')->comment = "References users.id for session owner";
+			$table->boolean('closed')->comment = "0 = no, 1 = yes";
+			$table->string('reference',255)->comment = "Check or EOB number";
 			$table->date('check_date');
 			$table->date('deposit_date');
 			$table->decimal('pay_total',12,2);
