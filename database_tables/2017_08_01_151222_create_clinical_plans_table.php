fcbcc593f6a6dd8fff9442ef7fef1da1b84ee7ae
Chandima Jayawickrama <chandimaerandani.ce@gmail.com>
2017-09-28 19:51:19 +0530

    Update 2017_08_01_151222_create_clinical_plans_table.php

--- database_tables/2017_08_01_151222_create_clinical_plans_table.php
+++ database_tables/2017_08_01_151222_create_clinical_plans_table.php
@@ -7,20 +7,21 @@ class CreateClinicalPlansTable extends Migration
 {
     /**
      * Run the migrations.
-     *
+     * Creates database table clinical_plans
+     * @author Chandima Jayawickrama
      * @return void
      */
     public function up()
     {
         Schema::create('clinical_plans',function(Blueprint $table){
 			$table->engine = 'InnoDB';
-			$table->string('id',31);
-			$table->bigInteger('pid')->default(0);
-			$table->boolean('normal_flag');
-			$table->boolean('cqm_flag');
-			$table->boolean('cqm_2011_flag');
-			$table->boolean('cqm_2014_flag');
-			$table->string('cqm_measure_group',10);
+			$table->string('id',31)->comment = "Unique and maps to list clinical_plans";
+			$table->bigInteger('pid')->default(0)->comment = "0 is default for all patients, while > 0 is id from patient_data table";
+			$table->boolean('normal_flag')->comment = "Normal Activation Flag";
+			$table->boolean('cqm_flag')->comment = "Clinical Quality Measure flag (unable to customize per patient)";
+			$table->boolean('cqm_2011_flag')->comment = "2011 Clinical Quality Measure flag (unable to customize per patient)";
+			$table->boolean('cqm_2014_flag')->comment = "2014 Clinical Quality Measure flag (unable to customize per patient)";
+			$table->string('cqm_measure_group',10)->comment = "Clinical Quality Measure Group Identifier";
 			$table->primary(array('id','pid'));
 			
 		});
